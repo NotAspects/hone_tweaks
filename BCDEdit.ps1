f94639c162310fe4a69d45d1da741994af3bc996
Aspect <73410229+NotAspects@users.noreply.github.com>
2023-04-24 19:04:41 +0200

    Add files via upload

---
+++ BCDEdit.ps1
@@ -0,0 +1,30 @@
+function BCDEdit {
+
+  Start-Process -FilePath bcdedit.exe -ArgumentList "/set tscsyncpolicy enhanced"
+  Start-Process -FilePath bcdedit.exe -ArgumentList "/timeout 0"
+  Start-Process -FilePath bcdedit.exe -ArgumentList "/set bootux disabled"
+  Start-Process -FilePath bcdedit.exe -ArgumentList "/set bootmenupolicy standard"
+  Start-Process -FilePath bcdedit.exe -ArgumentList "/set quietboot yes"
+if ((Get-WmiObject Win32_Processor).Name -like '*Intel*') {
+
+  Start-Process -FilePath bcdedit.exe -ArgumentList "/set nx optout"
+
+} else {
+
+  Start-Process -FilePath bcdedit.exe -ArgumentList "/set nx alwaysoff"
+
+}
+Start-Process -FilePath bcdedit.exe -ArgumentList "/set allowedinmemorysettings 0x0"
+Start-Process -FilePath bcdedit.exe -ArgumentList "/set vsmlaunchtype Off"
+Start-Process -FilePath bcdedit.exe -ArgumentList "/set vm No"
+Set-ItemProperty -Path 'HKLM\Software\Policies\Microsoft\FVE' -Name 'DisableExternalDMAUnderLock' -Type DWord -Value 0 -Force
+Set-ItemProperty -Path 'HKLM\Software\Policies\Microsoft\Windows\DeviceGuard' -Name 'EnableVirtualizationBasedSecurity' -Type DWord -Value 0 -Force
+Set-ItemProperty -Path 'HKLM\Software\Policies\Microsoft\Windows\DeviceGuard' -Name 'HVCIMATRequired' -Type DWord -Value 0 -Force
+Start-Process -FilePath bcdedit.exe -ArgumentList "/set x2apicpolicy Enable"
+Start-Process -FilePath bcdedit.exe -ArgumentList "/set uselegacyapicmode No"
+Start-Process -FilePath bcdedit.exe -ArgumentList "/set configaccesspolicy Default"
+Start-Process -FilePath bcdedit.exe -ArgumentList "/set usephysicaldestination No"
+Start-Process -FilePath bcdedit.exe -ArgumentList "/set usefirmwarepcisettings No "
+}
+
+BCDEdit
